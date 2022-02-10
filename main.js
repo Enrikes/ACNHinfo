4ae4738ce2c2384a85ed3b873e5a2472d590fec3
Enrique Troche <55448347+Enrikes@users.noreply.github.com>
2022-02-10 18:13:38 -0500

    Added overlay effect.

--- main.js
+++ main.js
@@ -7,17 +7,31 @@ async function getFishIcon(wrapper) {
     const fishPic = fishIcon["icon_uri"];
     const icon = document.createElement("div");
     icon.classList.add("fish-item");
-    icon.innerHTML = `<img src='${fishPic}'>`;
+    icon.innerHTML = `<img class='loading' src='${fishPic}'>`;
     wrapper.appendChild(icon);
   }
 }
 function displayFishInfo() {
-  const fishBlur = document.createElement("div").classList.add("fish-blur");
+  const fishBlur = document.createElement("div");
+  fishBlur.classList.add("fish-blur");
+  document.body.style.overflow = "hidden";
   document.body.appendChild(fishBlur);
+  const xMark = document.createElement("div");
+  xMark.classList.add("x-mark");
+  xMark.innerHTML = `<img src='img/overaly/x.png'>`;
+  xMark.addEventListener("click", removeDisplayFishInfo);
+  // Removes Fish information
+  function removeDisplayFishInfo() {
+    xMark.remove();
+    fishBlur.remove();
+    document.body.style.removeProperty("overflow");
+  }
+  fishBlur.appendChild(xMark);
   console.log("i have been clicked");
 }
 function fishClick() {
   fishWrapper.addEventListener("click", displayFishInfo);
 }
+
 fishClick();
 getFishIcon(fishWrapper);

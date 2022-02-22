722b5e5557c85650e552c6cfb85783310fc95f56
Enrique Troche <55448347+Enrikes@users.noreply.github.com>
2022-02-22 13:42:12 -0500

    Displays month creature is caught in.

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 const fishWrapper = document.querySelector(".fish-icons");
-
 async function getFishIcon(wrapper) {
   const res = await fetch("https://acnhapi.com/v1a/fish/");
   const data = await res.json();
@@ -7,15 +6,12 @@ async function getFishIcon(wrapper) {
     const fishPic = fishIcon["icon_uri"];
     const fishId = fishIcon["id"];
     const fishName = fishIcon["name"]["name-USen"];
-
     const icon = document.createElement("div");
     icon.classList.add("fish-item");
     icon.innerHTML = `<img class='loading' src='${fishPic}'>`;
     wrapper.appendChild(icon);
-
     icon.addEventListener("mouseover", grabFishName);
     icon.addEventListener("mouseout", grabFishNameOut);
-
     function grabFishName() {
       const fishNamePlate = document.createElement("div");
       fishNamePlate.classList.add("fish-name-plate");
@@ -46,7 +42,6 @@ async function grabFishUrl(url) {
   console.log(data);
   return data;
 }
-
 async function displayFishInfo(url) {
   const fishUrl = await grabFishUrl(url);
   console.log(fishUrl);
@@ -68,7 +63,6 @@ async function displayFishInfo(url) {
     fishInfo.innerHTML = `<h1 class='fish-title'>${fishName}</h1>
   <img id='fish-img' src='${fish}'>  
   `;
-
     const monthGrid = document.createElement("div");
     monthGrid.setAttribute("id", "month-grid");
     fishInfo.appendChild(monthGrid);
@@ -86,13 +80,72 @@ async function displayFishInfo(url) {
       "November",
       "December",
     ];
+
     for (const month of monthArray) {
       const monthDom = document.createElement("div");
-      monthDom.setAttribute("id", "months");
+      monthDom.classList.add(month);
       monthDom.innerHTML = month;
       monthGrid.appendChild(monthDom);
+      monthDom.style.textAlign = "center";
+      monthDom.style.border = "1px";
+      monthDom.style.borderStyle = "solid";
+      monthDom.style.borderColor = "black";
+      monthDom.style.backgroundColor = "bisque";
     }
     fishBlur.appendChild(fishInfo);
+    // Month detector
+    const fishMonth = fishUrl["availability"]["month-array-northern"];
+    function monthDetector() {
+      if (fishMonth.includes(1)) {
+        const jan = document.querySelector(".January");
+        jan.style.backgroundColor = "red";
+      }
+      if (fishMonth.includes(2)) {
+        const feb = document.querySelector(".February");
+        feb.style.backgroundColor = "red";
+      }
+      if (fishMonth.includes(3)) {
+        const feb = document.querySelector(".March");
+        feb.style.backgroundColor = "red";
+      }
+      if (fishMonth.includes(4)) {
+        const feb = document.querySelector(".April");
+        feb.style.backgroundColor = "red";
+      }
+      if (fishMonth.includes(5)) {
+        const feb = document.querySelector(".May");
+        feb.style.backgroundColor = "red";
+      }
+      if (fishMonth.includes(6)) {
+        const feb = document.querySelector(".June");
+        feb.style.backgroundColor = "red";
+      }
+      if (fishMonth.includes(7)) {
+        const feb = document.querySelector(".July");
+        feb.style.backgroundColor = "red";
+      }
+      if (fishMonth.includes(8)) {
+        const feb = document.querySelector(".August");
+        feb.style.backgroundColor = "red";
+      }
+      if (fishMonth.includes(9)) {
+        const feb = document.querySelector(".September");
+        feb.style.backgroundColor = "red";
+      }
+      if (fishMonth.includes(10)) {
+        const feb = document.querySelector(".October");
+        feb.style.backgroundColor = "red";
+      }
+      if (fishMonth.includes(11)) {
+        const feb = document.querySelector(".November");
+        feb.style.backgroundColor = "red";
+      }
+      if (fishMonth.includes(12)) {
+        const feb = document.querySelector(".December");
+        feb.style.backgroundColor = "red";
+      }
+    }
+    monthDetector();
   };
 
   xMark.addEventListener("click", removeDisplayFishInfo);
@@ -102,16 +155,11 @@ async function displayFishInfo(url) {
     fishBlur.remove();
     document.body.style.removeProperty("overflow");
   }
-
   fishBlur.appendChild(xMark);
 }
-
-function collectionMode() {}
-
 function grabFishInfo() {}
 function fishClick() {
   fishWrapper.addEventListener("click", grabFishInfo);
 }
-
 fishClick();
 getFishIcon(fishWrapper);

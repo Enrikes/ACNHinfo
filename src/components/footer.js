fdb8c1a64283660e702edbd89b48f84e463b7353
Enrique Troche <55448347+Enrikes@users.noreply.github.com>
2022-09-07 23:44:46 -0400

    Fixed footer.

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,7 +1,24 @@
+import React from "react";
+import { KofiButton } from "react-kofi-button";
+
 export default function Footer() {
   return (
     <footer id="footer">
-      <p>This is a footah</p>
+      <div className="footer-container">
+        <div className="footer-col-1">
+          <p>links</p>
+        </div>
+        <div className="footer-col-2">
+          <p>copy right</p>
+        </div>
+        <div className="footer-col-3">
+          {" "}
+          <KofiButton
+            username="twentyfifthcorn"
+            label="Support me!"
+          ></KofiButton>{" "}
+        </div>
+      </div>
     </footer>
   );
 }

6739091461b90d38fda7555aeca32197eddfbeb2
Enrique Troche <55448347+Enrikes@users.noreply.github.com>
2022-09-16 00:41:53 -0400

    Removed redundant code.

--- src/components/registerButton.js
+++ src/components/registerButton.js
@@ -1,9 +1,7 @@
 import React from "react";
 export default function RegisterButton({ registerClick }) {
-  console.log(registerClick);
   return (
     <div className="register-wrapper">
-      <h1 id="register">Need an account?</h1>
       <button id="login-submit" onClick={registerClick}>
         Register
       </button>

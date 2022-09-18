d9a1426bfe2c74a37c621017dcdaedd08edb3eee
Enrique Troche <55448347+Enrikes@users.noreply.github.com>
2022-09-17 22:14:29 -0400

    Create loadingGrid.js

---
+++ src/components/loadingGrid.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Card from "./creatureCards";
+import imageUrl from "../img/grid/loading.png"
+
+export default function LoadingGrid({}){
+    const loadingItems = [1, 2, 3, 4, 5, 6, 7,8,9,10,11,12,13,14,15,1,1,1,1,1,1,1,1,1,1,1,1,1,1,11,1,1,1,1,1,1,1,1,1,1,1,1,1,];
+    const loadingCards = loadingItems.map((items) => {
+        return <Card name={items} icon={imageUrl} />;
+    })
+
+    return loadingCards
+}

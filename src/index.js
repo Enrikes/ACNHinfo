fc6b1cb2e3b1ed6c0dc5d3f0ec405482994c0ab9
Enrique Troche <55448347+Enrikes@users.noreply.github.com>
2022-10-21 23:31:35 -0400

    FEAT: Added Foldable phone CSS library.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./App.css";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './App.css';
+import App from './App';
+import '../node_modules/viewportsegments-css-polyfill/build/viewportsegments-css-polyfill.js';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);

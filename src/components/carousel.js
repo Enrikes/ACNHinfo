d1a27c669ee8cbcb007da6e9708f5322015070aa
Enrique Troche <55448347+Enrikes@users.noreply.github.com>
2022-11-22 23:54:21 -0500

    Feat: Villager cards display house furniture.

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -1,5 +1,18 @@
 import React from 'react';
 
 export default function Carousel({ furniture }) {
-  //   console.log(furniture);
+  const carouselContainer = furniture.map((itemArray) => {
+    const furnitureImage = itemArray;
+
+    const image = furnitureImage?.map((single) => {
+      console.log(single);
+      return single;
+    });
+    return (
+      <div className='carousel-item'>
+        <img src={image}></img>
+      </div>
+    );
+  });
+  return <div className='carousel'>{carouselContainer}</div>;
 }

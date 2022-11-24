1b71237832d45db8a197c0bbffc872cf6d759d28
Enrique Troche <55448347+Enrikes@users.noreply.github.com>
2022-11-24 08:29:45 -0500

    Chore: Removed test code.

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -1,16 +1,11 @@
 import React from 'react';
 
 export default function Carousel({ furniture }) {
-  console.log(furniture);
   const carouselContainer = furniture.map((itemArray) => {
     const villagerFurniture = itemArray.variations ?? [];
     const firstVariation = villagerFurniture[0]?.image;
     const content = firstVariation ?? '';
 
-    // const image = itemArray?.map((single) => {
-    //   console.log(single);
-    //   return single;
-    // });
     return (
       <div className='carousel-item'>
         <img src={content}></img>

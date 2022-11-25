f02d18dca4021db19e95afebca278e1b9b3b2812
Enrique Troche <55448347+Enrikes@users.noreply.github.com>
2022-11-24 19:18:59 -0500

    Fix: Array structure not displaying image.
    Due to some of items in the game not having a variation. The variation array will be null and display nothing.

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -5,9 +5,18 @@ export default function Carousel({ furniture }) {
     const villagerFurniture = itemArray.variations ?? [];
     const firstVariation = villagerFurniture[0]?.image;
     const content = firstVariation ?? '';
+    const furnitureName = itemArray.name;
+    if (content === '')
+      return (
+        <div className='carousel-item'>
+          <div className='furniture-name'>{furnitureName}</div>
+          <img src={itemArray.image}></img>
+        </div>
+      );
 
     return (
       <div className='carousel-item'>
+        <div className='furniture-name'>{furnitureName}</div>
         <img src={content}></img>
       </div>
     );

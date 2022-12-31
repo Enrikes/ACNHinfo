d52da1e7ffb2ffc3e633603b4f17905e058f0d09
Enrique Troche <55448347+Enrikes@users.noreply.github.com>
2022-12-30 23:30:35 -0500

    Chore: Added missing semicolon

--- src/components/loadingGrid.js
+++ src/components/loadingGrid.js
@@ -1,12 +1,15 @@
-import React from "react";
-import Card from "./creatureCards";
-import imageUrl from "../img/grid/loading.png"
+import React from 'react';
+import Card from './creatureCards';
+import imageUrl from '../img/grid/loading.png';
 
-export default function LoadingGrid({}){
-    const loadingItems = [1, 2, 3, 4, 5, 6, 7,8,9,10,11,12,13,14,15,1,1,1,1,1,1,1,1,1,1,1,1,1,1,11,1,1,1,1,1,1,1,1,1,1,1,1,1,];
-    const loadingCards = loadingItems.map((items) => {
-        return <Card name={items} icon={imageUrl} />;
-    })
+export default function LoadingGrid({}) {
+  const loadingItems = [
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 11, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+  ];
+  const loadingCards = loadingItems.map((items) => {
+    return <Card name={items} icon={imageUrl} />;
+  });
 
-    return loadingCards
+  return loadingCards;
 }

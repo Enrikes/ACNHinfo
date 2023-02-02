3327c6eeb08057d0cc3404486e1edb17a16ada81
Enrique Troche <55448347+Enrikes@users.noreply.github.com>
2023-02-02 16:09:07 -0500

    Feat: Created Dropdown Item CSS module.

--- src/components/dropdownitem.js
+++ src/components/dropdownitem.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import DropdownCSS from './dropdownitem.module.css';
 export default function DropdownItem(props) {
   return (
     <div
-      className='dropdown-item'
+      className={DropdownCSS['dropdown-item']}
       onClick={() => {
         props.function({ species: props.type, endpoint: props.endpoint });
       }}

98196f02c91aaf56b2f14131d071709f2e9b0c19
Enrique Troche <55448347+Enrikes@users.noreply.github.com>
2023-02-07 11:52:22 -0500

    Feat: Created month-grid CSS module.

--- src/components/monthGrid.js
+++ src/components/monthGrid.js
@@ -1,19 +1,20 @@
-import React from "react";
+import React from 'react';
+import CSS from './monthGrid.module.css';
 
 export default function MonthGrid({ months }) {
   const monthArray = [
-    "Jan",
-    "Feb",
-    "Mar",
-    "Apr",
-    "May",
-    "Jun",
-    "Jul",
-    "Aug",
-    "Sep",
-    "Oct",
-    "Nov",
-    "Dec",
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'Jun',
+    'Jul',
+    'Aug',
+    'Sep',
+    'Oct',
+    'Nov',
+    'Dec',
   ];
   const newMonths = months.map(function (value) {
     return value - 1;
@@ -21,16 +22,16 @@ export default function MonthGrid({ months }) {
   const displayGrid = monthArray.map((month, index) => {
     if (newMonths.includes(index)) {
       return (
-        <div key={month} className="month">
-          <div className="month-selector">{month}</div>
+        <div key={month} className={CSS.month}>
+          <div className={CSS['month-selector']}>{month}</div>
         </div>
       );
     }
     return (
-      <div key={month} className="month">
-        <div id="month-idle">{month}</div>
+      <div key={month} className={CSS.month}>
+        <div id={CSS['month-idle']}>{month}</div>
       </div>
     );
   });
-  return <div id="month-grid">{displayGrid}</div>;
+  return <div id={CSS['month-grid']}>{displayGrid}</div>;
 }

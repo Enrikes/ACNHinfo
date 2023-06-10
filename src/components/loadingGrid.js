064277dacc77f8f510b38fc06d2e7e1795ab87d8
Enrique Troche <55448347+Enrikes@users.noreply.github.com>
2023-06-09 20:35:53 -0400

    Fix: Skeleton will display 80 items.
    This is about the average amount creatures/villagers for every category. This way loading looks smoother.

--- src/components/loadingGrid.js
+++ src/components/loadingGrid.js
@@ -1,15 +1,13 @@
-import React from 'react';
-import Card from './creatureCards';
-import imageUrl from '../img/grid/loading.png';
+import React from "react";
+import Card from "./creatureCards";
+import imageUrl from "../img/grid/loading.png";
+import SkeletonCard from "../skeleton/skeletonCard";
 
 export default function LoadingGrid({}) {
-  const loadingItems = [
-    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 1, 1, 1, 1, 1, 1, 1, 1,
-    1, 1, 1, 1, 1, 1, 11, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-  ];
-  const loadingCards = loadingItems.map((items) => {
-    return <Card name={items} icon={imageUrl} />;
-  });
+  const loadingCards = [];
+  for (let i = 0; i < 80; i++) {
+    loadingCards.push(<SkeletonCard />);
+  }
 
   return loadingCards;
 }

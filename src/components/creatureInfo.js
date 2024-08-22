a95d5db123d67cabacf7ab0dc431148e860f29b3
Enrique Troche <55448347+Enrikes@users.noreply.github.com>
2024-08-22 16:32:43 -0400

    Chore: Removed unused prop drill

--- src/components/creatureInfo.js
+++ src/components/creatureInfo.js
@@ -1,19 +1,18 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-import MonthGrid from "./monthGrid";
-import Timeline from "./timeLine";
-import VillagerInfo from "./villagerInfo";
-import xMark from "../img/overaly/exit-button.png";
-import Loading from "./loading";
-import CardCSS from "./creatureInfo.module.css";
-import TimelineCSS from "./timeline.module.css";
-import { useQuery } from "@tanstack/react-query";
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import MonthGrid from './monthGrid';
+import Timeline from './timeLine';
+import VillagerInfo from './villagerInfo';
+import xMark from '../img/overaly/exit-button.png';
+import Loading from './loading';
+import CardCSS from './creatureInfo.module.css';
+import TimelineCSS from './timeline.module.css';
+import { useQuery } from '@tanstack/react-query';
 
 export default function CreatureInfo({
   cardInfo,
   toggleIsCreatureInfoShown,
   url,
-  handleFetchData,
   isVillagerActive,
   isCreatureActive,
 }) {
@@ -24,28 +23,28 @@ export default function CreatureInfo({
     enableScroll();
   }
   function disableScroll() {
-    document.body.style.overflow = "hidden";
+    document.body.style.overflow = 'hidden';
   }
   disableScroll();
   function enableScroll() {
-    document.body.style.overflow = "scroll";
+    document.body.style.overflow = 'scroll';
   }
   const endpoints = [
-    "/villager",
-    "villagerType",
-    "villagerPersonality",
-    "villagerHobby",
+    '/villager',
+    'villagerType',
+    'villagerPersonality',
+    'villagerHobby',
   ];
 
   const fetchVillager = async () => {
-    const { data } = await axios.get("/singleVillager", {
+    const { data } = await axios.get('/singleVillager', {
       params: { name: cardInfo },
     });
     const res = data;
     return res;
   };
   const fetchCreature = async () => {
-    const { data } = await axios.get("/singleCreature", {
+    const { data } = await axios.get('/singleCreature', {
       params: { name: cardInfo },
     });
     const res = data;
@@ -56,12 +55,12 @@ export default function CreatureInfo({
     data: villager,
     isLoading: villagerLoading,
     isFetching,
-  } = useQuery(["villager"], fetchVillager, {
+  } = useQuery(['villager'], fetchVillager, {
     enabled: isVillagerActive,
     refetchOnWindowFocus: false,
   });
   const { data: creature, isFetching: isCreatureFetching } = useQuery(
-    ["creature"],
+    ['creature'],
     fetchCreature,
     {
       enabled: isCreatureActive,
@@ -71,11 +70,11 @@ export default function CreatureInfo({
   );
   // Update title
   async function changeTitle() {
-    const nameArr = creature.name.split(" ");
+    const nameArr = creature.name.split(' ');
     for (var i = 0; i < nameArr.length; i++) {
       nameArr[i] = nameArr[i].charAt(0).toUpperCase() + nameArr[i].slice(1);
     }
-    const name = nameArr.join(" ");
+    const name = nameArr.join(' ');
 
     if (!isFetching) {
       document.title = `ACNHInfo - ${name}`;
@@ -84,13 +83,13 @@ export default function CreatureInfo({
   changeTitle();
 
   if (
-    url === "/villager" ||
-    url.endpoint === "villagerType" ||
-    url.endpoint === "villagerPersonality" ||
-    url.endpoint === "villagerHobby"
+    url === '/villager' ||
+    url.endpoint === 'villagerType' ||
+    url.endpoint === 'villagerPersonality' ||
+    url.endpoint === 'villagerHobby'
   ) {
     return isFetching ? (
-      <div className={CardCSS["background-blur"]}></div>
+      <div className={CardCSS['background-blur']}></div>
     ) : (
       <VillagerInfo
         villager={villager}
@@ -103,41 +102,41 @@ export default function CreatureInfo({
     const price = (150 / 100) * creature?.sell;
 
     return isCreatureFetching ? (
-      <div className={CardCSS["background-blur"]}>
+      <div className={CardCSS['background-blur']}>
         <Loading />
       </div>
     ) : (
-      <div className={"creature-container"}>
+      <div className={'creature-container'}>
         <div
-          className={CardCSS["background-blur"]}
+          className={CardCSS['background-blur']}
           onClick={(e) => {
             hideCreatureInfo(e);
           }}
         >
-          <div className={CardCSS["creature-info-container"]}>
-            <div className={CardCSS["creature-header"]}>
-              <h1 className={CardCSS["creature-title"]}>{creature.name}</h1>
+          <div className={CardCSS['creature-info-container']}>
+            <div className={CardCSS['creature-header']}>
+              <h1 className={CardCSS['creature-title']}>{creature.name}</h1>
               <div
-                className={CardCSS["x-mark-container"]}
+                className={CardCSS['x-mark-container']}
                 onClick={hideCreatureInfo}
               >
                 <img
-                  className={CardCSS["x-mark"]}
+                  className={CardCSS['x-mark']}
                   src={xMark}
                   onClick={hideCreatureInfo}
                 ></img>
               </div>
             </div>
             <img
-              id={CardCSS["creature-img"]}
+              id={CardCSS['creature-img']}
               src={creature.critterpediaImage}
             ></img>
-            <div className={CardCSS["creature-catch-phrase-container"]}>
-              <p id={CardCSS["creature-catch-phrase"]}>
+            <div className={CardCSS['creature-catch-phrase-container']}>
+              <p id={CardCSS['creature-catch-phrase']}>
                 {creature.catchPhrase}
               </p>
             </div>
-            <div className={CardCSS["availability"]}>
+            <div className={CardCSS['availability']}>
               <MonthGrid months={months} />
 
               <Timeline
@@ -146,20 +145,20 @@ export default function CreatureInfo({
               />
             </div>
 
-            <div className={CardCSS["creature-description"]}>
-              <div className={CardCSS["creature-desc-1"]}>
-                <div className={CardCSS["creature-desc-box"]}>
-                  <h1 className={CardCSS["creature-desc-title"]}>Location</h1>
-                  <div className={CardCSS["creature-desc-content"]}>
+            <div className={CardCSS['creature-description']}>
+              <div className={CardCSS['creature-desc-1']}>
+                <div className={CardCSS['creature-desc-box']}>
+                  <h1 className={CardCSS['creature-desc-title']}>Location</h1>
+                  <div className={CardCSS['creature-desc-content']}>
                     <p>{creature.whereHow}</p>
                   </div>
                 </div>
               </div>
 
-              <div className={CardCSS["creature-desc-2"]}>
-                <div className={CardCSS["creature-desc-box"]}>
-                  <h1 className={CardCSS["creature-desc-title"]}>Price</h1>
-                  <div className={CardCSS["creature-desc-content"]}>
+              <div className={CardCSS['creature-desc-2']}>
+                <div className={CardCSS['creature-desc-box']}>
+                  <h1 className={CardCSS['creature-desc-title']}>Price</h1>
+                  <div className={CardCSS['creature-desc-content']}>
                     <p>{creature.sell} Bells</p>
                     <p>Flick Price: {price} Bells</p>
                   </div>

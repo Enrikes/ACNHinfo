52e72b2a751ff4efc30004287768457c6690cee8
Enrique Troche <55448347+Enrikes@users.noreply.github.com>
2026-06-18 17:16:06 -0400

    Initial Header commit

---
+++ src/layout/Header.jsx
@@ -0,0 +1,18 @@
+import styles from './Header.module.css';
+export default function Header({}) {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a>Creatures</a>
+        </li>
+        <li>
+          <a>Fish</a>
+        </li>
+        <li>
+          <a>Villagers</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}

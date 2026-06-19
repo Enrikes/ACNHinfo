517abdd52aa70a37bbfb6e5a8561e64165767806
Enrique Troche <55448347+Enrikes@users.noreply.github.com>
2026-06-19 19:50:45 -0400

    Chore: Footer base shell.

---
+++ src/layout/Footer.jsx
@@ -0,0 +1,14 @@
+import styles from './Footer.module.css';
+export default function Footer({}) {
+  return (
+    <footer>
+      <div>
+        <p>ACNHinfo</p>
+      </div>
+      <div>
+        <p>Copyright 2026</p>
+      </div>
+      <div>Support me!</div>
+    </footer>
+  );
+}

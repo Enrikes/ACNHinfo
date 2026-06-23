eecf7ec72c9abdb5ba6e45cdda960ca3a257d56d
Enrique Troche <55448347+Enrikes@users.noreply.github.com>
2026-06-23 03:26:11 -0400

    Chore: Updated to import data from prop drilling.

--- src/components/catalog/CatalogGrid.jsx
+++ src/components/catalog/CatalogGrid.jsx
@@ -1,17 +1,15 @@
 import styles from './Catalog.module.css';
-import mockFish from '../../data/mockFish';
 
-export default function CatalogGrid({}) {
-  const cards = mockFish.map((item) => {
+export default function CatalogGrid({ data }) {
+  const cards = data.map((item) => {
     return (
       <div className={styles.itemContainer}>
         <div className={styles.itemCard}>
-          <div className={styles.itemIcon}>{item.icon}</div>
+          <div className={styles.itemIcon}><img src={item.iconImage}/></div>
         </div>
         <div className={styles.itemName}>{item.name}</div>
       </div>
     );
   });
-  console.log(mockFish);
   return <div className={styles.container}>{cards}</div>;
 }

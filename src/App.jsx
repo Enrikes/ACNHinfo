0d016548e6641df0a46eaed56684de8714141b2d
Enrique Troche <55448347+Enrikes@users.noreply.github.com>
2026-06-25 13:30:02 -0400

    Chore: Added routes for collections.

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,21 @@ import Header from './layout/Header';
 import Footer from './layout/Footer';
 import CatalogGrid from './components/catalog/CatalogGrid';
 import FishPage from './pages/fishPage.jsx';
+import InsectPage from './pages/insectPage.jsx';
+import SeaCreaturePage from './pages/seaCreaturePage.jsx';
+import VillagerPage from './pages/villagerPage.jsx';
+import { Routes, Route } from 'react-router';
 
 function App() {
   return (
     <>
       <Header></Header>
-      <FishPage />
+      <Routes>
+        <Route path='fish' element={<FishPage />} />
+        <Route path='insect' element={<InsectPage />} />
+        <Route path='sea-creature' element={<SeaCreaturePage />} />
+        <Route path='villager' element={<VillagerPage />} />
+      </Routes>
       <Footer></Footer>
     </>
   );

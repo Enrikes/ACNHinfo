770f5a8d51c56a5abc1f33d7d1e570915d81e788
Enrique Troche <55448347+Enrikes@users.noreply.github.com>
2026-07-12 02:07:03 -0400

    Chore: Updated query/HTML structure.

--- src/pages/detail/creatureDetail.jsx
+++ src/pages/detail/creatureDetail.jsx
@@ -1,13 +1,36 @@
 import { useParams } from 'react-router';
 import Return from '../../components/detail/returnButton';
 import styles from './creatureDetail.module.css';
+import Profile from '../../components/detail/profile';
+import { useQuery } from '@tanstack/react-query';
+import { GetSingleCreature } from '../../data/singleCreature';
 
 export default function CreatureDetail({}) {
   const { catergory, slug } = useParams();
-  return (
-    <div>
-      <Return basePath={catergory} />
-      <div className={styles.profile}></div>
-    </div>
-  );
+
+  const query = useQuery({
+    queryKey: ['creature'],
+    queryFn: () => GetSingleCreature(slug),
+  });
+
+  if (query.isPending === true) {
+    return <div>Loading...</div>;
+  } else {
+    const creature = query.data[0];
+    console.log(creature);
+    return (
+      <div>
+        <Return basePath={catergory} />
+        <div className={styles.container}>
+          <Profile
+            name={creature.name}
+            catchPhrase={creature.catchPhrase}
+            price={creature.sell}
+            location={creature.WhereHow}
+            image={creature.critterpediaImage}
+          />
+        </div>
+      </div>
+    );
+  }
 }
